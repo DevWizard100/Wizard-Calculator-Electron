7110811d25751a4ed812f41f3eff9d9c1b98ce7f
Wizardgamer100 <felixschalk04@outlook.com>
2022-12-16 18:49:48 +0100

    15.12.22

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ const createWindow = () => {
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
       transparent: true,
-      icon: path.join(__dirname, 'icon.png'),
+      icon: path.join(__dirname, 'icon/icon.png'),
       backgroundColor: '#312450',
     },
   });
